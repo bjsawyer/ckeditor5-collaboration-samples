a99b2dc53190ad2d521269c0640854231e0eb879
Brendan Sawyer <brendan.sawyer@excella.com>
2020-08-19 11:42:21 -0400

    Remove unneeded subprojects

--- letters/src/letters.js
+++
@@ -1,8 +0,0 @@
-/**
- * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
- * This file is licensed under the terms of the MIT License (see LICENSE.md).
- */
-
-import Letters from '@ckeditor/letters/src/letters';
-
-export default Letters;
