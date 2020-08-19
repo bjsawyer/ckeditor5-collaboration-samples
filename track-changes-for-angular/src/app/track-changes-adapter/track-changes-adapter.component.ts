f7b4278b1f555cc02c7f4c8da129bf321ceed365
Brendan Sawyer <brendan.sawyer@excella.com>
2020-08-19 14:55:00 -0400

    More cleanup

--- track-changes-for-angular/src/app/track-changes-adapter/track-changes-adapter.component.ts
+++ track-changes-for-angular/src/app/track-changes-adapter/track-changes-adapter.component.ts
@@ -10,7 +10,7 @@ import {
 } from '@angular/core'
 import { CKEditor5 } from '@ckeditor/ckeditor5-angular'
 
-import * as ClassicEditorBuild from '../../../vendor/ckeditor5/build/classic-editor-with-track-changes.js'
+import * as ClassicEditorWithTrackChanges from '../../../vendor/ckeditor5/build/classic-editor-with-track-changes.js'
 import { initialHtml, intialData } from './data'
 import { getTrackChangesAdapter } from './track-changes-adapter'
 
@@ -21,29 +21,25 @@ import { getTrackChangesAdapter } from './track-changes-adapter'
 })
 export class TrackChangesAdapterComponent implements OnInit, AfterViewInit, OnDestroy {
   @Output() ready = new EventEmitter<CKEditor5.Editor>()
-  Editor = ClassicEditorBuild
+  CustomEditorBuild = ClassicEditorWithTrackChanges
   editor?: CKEditor5.Editor
-  data = initialHtml
-
+  intialHtml = initialHtml
   private readonly licenseKey = 'ckeditor-license-key'
   private licenseValue = '1R5JdtfXfK7Ji9wfJGazaiv/BdyIK6/IoGa1g7VzQoC8czRaSJCcygo='
-
   // Note that Angular refs can be used once the view is initialized so we need to create
   // this container and use in the above editor configuration to work around this problem.
   private sidebar = document.createElement('div')
-
   private boundRefreshDisplayMode = this.refreshDisplayMode.bind(this)
   private boundCheckPendingActions = this.checkPendingActions.bind(this)
   @ViewChild('sidebar') private sidebarContainer?: ElementRef<HTMLDivElement>
 
-  ngOnInit() {
+  ngOnInit(): void {
     // Save the provided license key in the local storage.
-    this.licenseValue =
-      window.localStorage.getItem(this.licenseKey) || window.prompt('Your license key')
+    this.licenseValue = window.localStorage.getItem(this.licenseKey)
     window.localStorage.setItem(this.licenseKey, this.licenseValue)
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit(): void {
     if (!this.sidebarContainer) {
       throw new Error('Div container for sidebar was not found')
     }
@@ -51,39 +47,36 @@ export class TrackChangesAdapterComponent implements OnInit, AfterViewInit, OnDe
     this.sidebarContainer.nativeElement.appendChild(this.sidebar)
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     window.removeEventListener('resize', this.boundRefreshDisplayMode)
     window.removeEventListener('beforeunload', this.boundCheckPendingActions)
   }
 
-  onReady(editor: CKEditor5.Editor) {
+  onReady(editor: CKEditor5.Editor): void {
     this.editor = editor
     this.ready.emit(editor)
-
     // Make the track changes mode the "default" state by turning it on right after the editor initializes.
     this.editor.execute('trackChanges')
-
     // Prevent closing the tab when any action is pending.
     window.addEventListener('beforeunload', this.boundCheckPendingActions)
-
     // Switch between inline and sidebar annotations according to the window size.
     window.addEventListener('resize', this.boundRefreshDisplayMode)
     this.refreshDisplayMode()
   }
 
-  resetLicenseKey() {
+  resetLicenseKey(): void {
     window.localStorage.removeItem(this.licenseKey)
     window.location.reload()
   }
 
-  private checkPendingActions(domEvt) {
+  private checkPendingActions(event: Event): void {
     if (this.editor.plugins.get('PendingActions').hasAny) {
-      domEvt.preventDefault()
-      domEvt.returnValue = true
+      event.preventDefault()
+      event.returnValue = true
     }
   }
 
-  private refreshDisplayMode() {
+  private refreshDisplayMode(): void {
     const annotations = this.editor.plugins.get('Annotations')
     const sidebarElement = this.sidebarContainer.nativeElement
 
@@ -101,7 +94,7 @@ export class TrackChangesAdapterComponent implements OnInit, AfterViewInit, OnDe
     }
   }
 
-  get editorConfig() {
+  get editorConfig(): object {
     return {
       extraPlugins: [getTrackChangesAdapter(intialData)],
       sidebar: {
